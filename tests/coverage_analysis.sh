0743dabb4add0ed6e3430761f90a8ead2e60b461
Charles Benca <zzazzbug@gmail.com>
2026-07-30 23:02:37 -0400

    added missing test coverage detection capability

---
+++ tests/coverage_analysis.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd);
+cd "$SCRIPT_DIR";
+
+
+
+main() {
+	local coverage_file=$(mktemp);
+	export RRSSH_COVERAGE_FILE="$coverage_file";
+
+	../with-path.sh bats *.fast.bats;
+
+	grep -Po '\bcoverage id_[a-zA-Z0-9_\-]+\b' ../rrssh | awk '{ print $2; }' |
+		while IFS="" read -r line || [ -n "$line" ]; do
+			grep -xFq "$line" "$coverage_file" || {
+				echo "Not found: $line";
+			};
+
+		done;
+
+	rm -f "$coverage_file";
+}
+
+main "$@";
